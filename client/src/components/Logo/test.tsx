d2e9713fc197f6f836ed5a8744a360c4aee8e833
Cicero Hiago Santos <CHISANTOS>
2022-01-15 13:23:41 -0300

    feat: add responsive logo

--- client/src/components/Logo/test.tsx
+++ client/src/components/Logo/test.tsx
@@ -11,12 +11,36 @@ describe('<Logo />', () => {
       color: theme.colors.white
     })
   })
-
   it('should render a black label when color is passed', () => {
     renderWithTheme(<Logo color="black" />)
 
     expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
       color: theme.colors.black
     })
+  })
+  it('should render a normal logo when size is default ', () => {
+    renderWithTheme(<Logo size="normal" />)
+
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: '11rem'
+    })
+  })
+  it('should render a bigger logo', () => {
+    renderWithTheme(<Logo size="large" />)
+
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: '20rem'
+    })
+  })
+  it('should render a bigger logo without text if hideOnMobile', () => {
+    renderWithTheme(<Logo hideOnMobile />)
+
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyleRule(
+      'width',
+      '5.8rem',
+      {
+        media: '(max-width: 768px)'
+      }
+    )
   })
 })
