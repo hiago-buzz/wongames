a06464dc82706977a51220bb14e5b2a20fe98195
Cicero Hiago Santos <CHISANTOS>
2022-01-15 15:42:36 -0300

    feat: create base button

---
+++ client/src/components/Button/stories.tsx
@@ -0,0 +1,13 @@
+import { Meta, Story } from '@storybook/react/types-6-0'
+import Button, { ButtonProps } from '.'
+
+export default {
+  title: 'Button',
+  component: Button
+} as Meta
+
+export const Default: Story<ButtonProps> = (args) => <Button {...args} />
+
+Default.args = {
+  children: 'Buy now'
+}
