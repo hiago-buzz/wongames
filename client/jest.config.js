926e0500890835e8e9859b57cb0f39bb31ea568c
Cicero Hiago Santos <CHISANTOS>
2022-01-15 12:34:32 -0300

    chore: config absolute path in jest

--- client/jest.config.js
+++ client/jest.config.js
@@ -3,9 +3,11 @@ module.exports = {
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'],
-  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
-  // moduleNameMapper: {
-  //   'styled-components':
-  //     '<rootDir>/node_modules/styled-components/dist/styled-components.browser.cjs.js'
-  // }
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
+  moduleNameMapper: {
+    '^styles/(.*)$': '<rootDir>/src/styles/$1',
+    '^utils/(.*)$': '<rootDir>/src/utils/$1'
+    // 'styled-components':
+    //   '<rootDir>/node_modules/styled-components/dist/styled-components.browser.cjs.js'
+  }
 }
